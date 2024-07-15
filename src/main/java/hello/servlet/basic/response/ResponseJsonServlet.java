79a18e6e9fef865da308a033ff62ffe1a53c4dcb
hwlee9905 <9905hyun@naver.com>
2024-07-15 22:34:44 +0900

    study commit

--- src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
+++ src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -7,18 +7,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.HandlerAdapter;
-import org.springframework.web.servlet.HandlerExecutionChain;
-import org.springframework.web.servlet.ModelAndView;
-
 import java.io.IOException;
-import java.util.Locale;
-
 @WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
 public class ResponseJsonServlet extends HttpServlet {
     private ObjectMapper objectMapper = new ObjectMapper();
